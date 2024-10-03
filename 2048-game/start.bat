b0b0fcc58dc2e06cc1069c84e52e631dfc8617ed
Welma4 <124872550+Welma4@users.noreply.github.com>
2024-10-03 19:27:02 +0300

    Add files via upload

---
+++ 2048-game/start.bat
@@ -0,0 +1,15 @@
+@echo off
+
+docker build -t 2048-game .
+
+docker stop game1
+docker rm game1
+echo Building first container...
+docker run -d --name game1 -p 8080:8080 2048-game
+
+docker stop game2
+docker rm game2
+echo Building second container...
+docker run -d --name game2 -p 8090:8080 2048-game
+
+pause
